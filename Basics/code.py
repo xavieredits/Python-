1e009d7e0d0dae249098695cca1c489974af9688
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2024-11-28 16:52:07 +0000

    code basics

--- Basics/code.py
+++ Basics/code.py
@@ -1,6 +1,10 @@
+# some basics about puthon
 print("hello");
 
 def code(n):      
     print(n)
-
+#  defines someting can be imported into other files
+#  like forom code import code
+# cod("string or whatever")
 code(4)
+
