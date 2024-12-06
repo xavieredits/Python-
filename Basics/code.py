99597a201fcaf892573c8c01ada2e83d5c09cbd8
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2024-12-06 17:05:16 +0000

    code

--- Basics/code.py
+++ Basics/code.py
@@ -8,3 +8,18 @@ def code(n):
 # cod("string or whatever")
 code(4)
 
+code("lemon milk")
+
+
+# ###################################### python shells and and codes of prompt screen #############################################
+
+# by writing " python " in terminal you can access python shell to code 
+# to exti the shell press ctrl +z and ctrl + d
+#  ctrl + z is used to suspend the shell for the current but the shell can be use again \
+# ctrl + d is used to fully terminate the shell 
+#     note : the code writen in shell is not saved as well as the writen code is executed immediately after it is writen 
+
+
+# creating a loop in python 
+for c in "chai":# some space is left after the code that is indentatin represented by the "..." in vs code
+    print(c)
