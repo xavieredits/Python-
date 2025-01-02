742a79e693e5c3c39ca05995d61b15fdacdf1ec8
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-01-02 16:06:01 +0000

    pact

--- Basics/pact.py
+++ Basics/pact.py
@@ -15,9 +15,9 @@
 # else:
 #     print("odd");
 
-# rows = int(input("Enter number of rows: "))
-# for i in range(rows):
-#     for j in range(i+1):
-#         print("* ", end="")
-#     print()
+rows = int(input("Enter number of rows:"))
+for i in range(rows):
+    for j in range(i+1):
+        print("* ",end="")
+    print()
 
