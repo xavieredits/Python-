a7ca626e8291c9a028a354d5aaa302339c271be5
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-05 14:22:52 +0000

    lessone 1

---
+++ lesson_01.py
@@ -0,0 +1,19 @@
+#  variables 
+a =1
+b=3
+print( a+b);
+# veriables works as containers
+
+# datatypes
+
+a= 1 #here a is an integer
+b=5.334 # here b is float
+
+c ="xani" # c is string 
+d = False # boolean datatype 
+
+e = none # is a none type veriable 
+
+# false and none are diff/.....
+
+ 
