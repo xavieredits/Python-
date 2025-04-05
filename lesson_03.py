55406092c22dc277550716d8c60b703b40c8b6da
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-05 15:38:48 +0000

    lesson 3

---
+++ lesson_03.py
@@ -0,0 +1,5 @@
+name = "xani";
+# string is immutable , we can not do customiszation on character level after creating a string
+
+nameShort = name[0:3] #start form index 0 to 3 excluding 3 which is "xan"
+print(nameShort);
