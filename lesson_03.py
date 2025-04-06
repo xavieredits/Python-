d98f1e01792faa318e4f02ca31222ed9335654f5
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-06 08:40:00 +0000

    lesson3

--- lesson_03.py
+++ lesson_03.py
@@ -1,5 +1,15 @@
+# slicing 
 name = "xani";
 # string is immutable , we can not do customiszation on character level after creating a string
 
 nameShort = name[0:3] #start form index 0 to 3 excluding 3 which is "xan"
 print(nameShort);
+
+anotherName =name[1:3]
+print(anotherName);
+
+# negative/ reverse slicing
+
+choosenName = "harry"
+# userName =choosenName[-4:-2]
+# print(userName);
