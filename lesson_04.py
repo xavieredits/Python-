f61d7d969925bdc755daf21eef36f2e3cf938b67
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-08 09:04:00 +0000

    lesson 4

---
+++ lesson_04.py
@@ -0,0 +1,12 @@
+# list and tuples
+# lists are mutable , used to store a set of value of any datatype
+
+data = ["akash",20 , "apple",5 ,20.36 , False , "xani"]
+print(type(data),data)
+# we can do changes in list but not in strings EXAMPLE:
+
+data[0] = "greps";
+print(data[0])
+print(data)
+
+
