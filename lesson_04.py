19dc930b9f217124b08ef8079737ce5f22dc9735
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-09 15:14:29 +0000

    lesson 4 till pop

--- lesson_04.py
+++ lesson_04.py
@@ -9,4 +9,23 @@ data[0] = "greps";
 print(data[0])
 print(data)
 
+# methods of lists
+# append ==> addtion of a new data in the end 
+data.append("harry");
+print(data);
 
+# sort ==> for arrangemnt of integers in assending order
+list1 = [1,3,45,72,35];
+list1.sort();
+print(list1);
+
+# reverse ==> descending order
+list1.reverse();
+print(list1); 
+
+# insert ==> inserting a value in between or at any perticular index of the list
+
+list1.insert(4,"value insert"); # here 4 is the value of index and the next value is what to be inserted 
+print(list1);
+
+# pop ==> to remove any data at any perticlar index
