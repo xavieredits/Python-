c5139b0f59f8b222ab56219871183c14f860feba
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-13 11:11:08 +0000

    lesson 5

--- lesson_05.py
+++ lesson_05.py
@@ -7,3 +7,39 @@ marks={
     "english":56
 }
 print(marks,type(marks));
+
+# soule use of dictonary to use key value pair 
+
+# indextation use in dict
+print(marks["english"]);
+
+# properties of python dicts
+# unorderd , mutable ,indexed , cannot contain duplicate keys 
+
+# methods of dictonary 
+
+anotherDict ={
+    "name":"xani",
+    "email":"xani@gmail.com",
+    "contact":989070987,
+    0:"enpid"
+};
+print(anotherDict);
+# dict items is use to get all the items in key value pair
+print(anotherDict.items());
+
+# .keys ==> to get keys from the dict
+print(anotherDict.keys());
+
+# .values is uesd to get all the values in the dict
+
+print(anotherDict.values());
+
+
+# [updating ditonary ]
+print(marks);
+marks.update({"science":55,"hindi":88});
+print(marks);
+
+# [get] ==> returns the value fo the sepecific key(and value is returned)
+
