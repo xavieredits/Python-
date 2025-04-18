f22931174bfe1d524dc052981d96f212e7d9e8e6
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-18 09:43:16 +0000

    conditions

---
+++ lesson_06.py
@@ -0,0 +1,20 @@
+# conditionals in python
+
+a = int(input("enter you age:"))
+if (a>=18):
+    print("you are elligible");
+else:
+    print("you are underage");
+
+print("end of program");
+
+# [elif]==> it is used in multi condition case
+
+age = int(input("enter you age:"));
+if(age>=18):
+    print("nice");
+elif(age<=0):
+    print("invalid")
+else:
+    print("meh!");
+
