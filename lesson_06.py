6a4f2055eab17580f30972c888d70a76284066a1
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-20 12:20:24 +0000

    lesson 6 done

--- lesson_06.py
+++ lesson_06.py
@@ -36,3 +36,17 @@ if(m>=18):
 
 # here every "if" statement is independent .... bit else and elif is depends on if .. cant stand alone
 
+print("============practice set==========");
+
+a1 =int(input("enter number 1: "))
+a2 =int(input("enter number 2: "))
+a3 =int(input("enter number 3: "))
+a4 =int(input("enter number 4: "))
+
+if(a1>a2 and a1>a3 and a1>a4):
+    print("number is a1",a1)
+if(a2>a1 and a2>a3 and a2>a4):
+    print("a2 is greater:",a2)
+# etc 
+
+
