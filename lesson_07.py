7feae8b42c382d7d9e05afc9bc0c9d3f5529d189
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-25 21:30:59 +0000

    4:09:42

--- lesson_07.py
+++ lesson_07.py
@@ -9,3 +9,21 @@ while(y<6):
     print(y)
     y+=1
 
+# this block continues to execute until the condition is false 
+
+# Q: write a program to print "xani" 5 times
+s = 0
+while(s<5):
+    print(s,"xani")
+    s = s+1
+
+# print elemets of a list using while loop
+
+list1 = [1,"xani" , "github", "sam" ,34 ,88]
+
+e = 0
+while(e<len(list1)):  #here the length of list is used for the range in while loop
+    print(list1[e])
+    e = e+1
+
+# [ for loops ]
