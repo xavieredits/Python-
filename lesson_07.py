4a53d6f81b23723cd1fc7b55a4bd78c539aca911
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-26 08:51:30 +0000

    4:25:12

--- lesson_07.py
+++ lesson_07.py
@@ -88,3 +88,16 @@ i= 0
 while(i<5):
     print(i)
     i=i+1
+
+print("========= practice set ==========")
+
+# Q [ write a program to print multiplication table of a given number using for loop. ]
+
+n = int(input("enter a number: "))
+
+for i in range(1,11):
+    print(f"{n} x {i} = {n*i}");
+
+# Q [write a program to great all the person name stored in a list 'l' and which start with s. ]
+
+l= ["harry" , "shoan" , "sachin" ,"rahul"]
