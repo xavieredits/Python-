02ad03ed33b583c8fc49f94305304d0370cb9cd0
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-26 09:07:50 +0000

    4:30:08

--- lesson_07.py
+++ lesson_07.py
@@ -114,3 +114,13 @@ i = 1
 while(i<11):
     print(n*i)
     i = i+1;
+
+# find if the number is prime of not 
+n = int(input("enter a number: "))
+
+for i in range(2,n):
+    if(n%i) == 0:
+        print("number is not prime")
+        break
+else:
+    print("number is prime")
