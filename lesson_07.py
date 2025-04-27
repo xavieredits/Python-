a355e57a753b0352b4f326835984bc6b6e19691c
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-27 22:12:14 +0000

    lesson 7 done

--- lesson_07.py
+++ lesson_07.py
@@ -165,4 +165,31 @@ for i in range(1, n+1):
     print("*"* (2*i-1), end="")  #adding a end statement helps to prevent any new line by the print command
     print("")
 
-#  
+#  Q [ write a prorgram to print the following patter ]
+'''
+*
+**
+***  for n = 3
+'''
+print("another time for the stars")
+
+n = int(input("enter the number: "))
+for i in range(1,n+1):
+    print("*"*i ,end="")
+    print("")
+
+#  Q [ write a prorgram to print the following patter ]
+'''
+***
+* *
+***  for n = 3
+'''
+n = int(input("enter the number: "))
+for i in range(1,n+1):
+    if(i==1 or i==n):
+        print("*"*n, end="")
+    else:
+        print("*",end="")
+        print(" "* (n-2), end="")
+        print("*",end="")
+    print("")
