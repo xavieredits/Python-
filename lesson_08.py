23167987c6d9e2e66f9e56f31dd327b7d9cdff0d
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-28 18:53:40 +0000

    5:07:40

--- lesson_08.py
+++ lesson_08.py
@@ -13,3 +13,31 @@ def avg():
 
 avg() # <=== calling the function 
 
+# [FUNCTION WITH ARRGUMNET ]
+
+def goodDay(name , ending):
+    print("good day, " + name)
+    print(ending)
+
+goodDay("harry" ,"thanks")  # harry goes into the name variable
+goodDay("xani" , "great job")
+
+
+# but : these functions do return a value to which is used to assigning to a new varriable like:
+
+def work(name ,status):
+    print("thanks for today " + name)
+    print(status)
+    return "done"
+
+a = work("neha" ,"loggedin") # here a is getting no value untill you mention return to the funtion
+print(a) # <== "done" is returend to the "a"
+
+# [ DEFAULT VALUE ARGUMNET ]
+
+def libWrok(name , status="available"):
+    print(f"the book name is {name} and the status is {status}")
+    return "thanks for checking"
+
+libWrok("fram" ,"available")
+libWrok("comics") # <== here the status is not provided but the default value will be used by the logic
