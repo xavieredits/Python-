59bdf11a38c16d8ac90e780491516d71294bfe77
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-04-28 11:33:16 +0000

    5:01:00

--- lesson_08.py
+++ lesson_08.py
@@ -1,11 +1,15 @@
-# [ FUNCTIONS AND RECURSIONS ]
+# [ FUNCTIONS ANS RECURSIONS ]
 
-# by creating a function you can give a name to the pice of logic
+# BY creating a function you can define a piece of program to use it again
 
+# FUNCTION DEFINITION
 def avg():
-    a = int(input("enter value 1: "))
-    b = int(input("enter value 2: "))
-    c = int(input("enter value 3: "))
+    a = int(input("enter value 1: ")) 
+    b = int(input("enter value 2: ")) 
+    c = int(input("enter value 3: ")) 
     
-    average = (a+b+c)/3
-    print(average);
+    average = (a + b + c)/3
+    print(average)
+
+avg() # <=== calling the function 
+
