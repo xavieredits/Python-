8d126f02589c4181509831fd39e8a5ca73643f7f
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-05-12 15:48:43 +0000

    5:46:12

--- Project1/main.py
+++ Project1/main.py
@@ -1,13 +1,15 @@
+import random
 # [ MAIN CODE ]
 
 # 1 for snake 
 # -1 for water
 # 0 for gun 
 
-computer = -1
+computer = random.choice([-1,0,1])
 youstr= input("enter your choice: ")
 youDict = {"s":1 , "w":-1 , "g":0}
 reverseDict = {1:"sanke" , -1:"water" , 0:"gun"}
+
 
 you= youDict[youstr]
 
