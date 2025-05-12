ba1f1432bfb38e5d31cd5c98f523967fbb3dd80e
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-05-12 15:56:54 +0000

    project 1 completed

--- Project1/main.py
+++ Project1/main.py
@@ -6,6 +6,8 @@ import random
 # 0 for gun 
 
 computer = random.choice([-1,0,1])
+# random has to be imorted so compter can choose random numbers
+
 youstr= input("enter your choice: ")
 youDict = {"s":1 , "w":-1 , "g":0}
 reverseDict = {1:"sanke" , -1:"water" , 0:"gun"}
@@ -13,7 +15,11 @@ reverseDict = {1:"sanke" , -1:"water" , 0:"gun"}
 
 you= youDict[youstr]
 
+# by now we have 2 numbers (variables) , you and computer
+ 
 print(f"you chose {reverseDict[you]}\ncomputer chose {reverseDict[computer]}")
+
+# conditioned lader 
 
 if(computer == you):
     print("its a draw")
