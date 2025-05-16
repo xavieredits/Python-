4254de9921fefb3d7652f24f34b1c2cd72828c4e
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2025-05-16 09:03:31 +0000

    6:00:45

--- lessons/lesson_09.py
+++ lessons/lesson_09.py
@@ -7,7 +7,10 @@ emails =[]
 ''' 
 #  here the file system comes in play, it help the executed file to store the file into the non volatile memory 
 
-f = open("file.txt")
+f = open("/workspaces/Python-/lessons/file.txt")
+# here the path of the file has to be mentioned 
 data = f.read()
 print(data)
 f.close()
+
+
